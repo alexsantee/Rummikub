f1e6a517d9f36a27fe0183ecd6d9d6502d0a6232
alexsantee <alexsantee@hotmail.com>
2018-06-17 11:04:11 -0300

    Funções de sacar carta e ler da memória

--- baralho.c
+++ baralho.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>	//Define ponteiro para NULL e funções aleatórias
+#include <stdio.h>	//Para comandos de arquivo e saída de erros
 #include <time.h>	//Fornece seed para funções aleatórias
 
 #define N_BARALHOS 2	//Numero de baralhos usados
@@ -8,14 +9,17 @@
 #define N_NAIPES 4	//Quantos naipes existem na próxima lista
 #define NAIPES {'!', '@', '#', '$'}	//Naipes utilizados
 #define N_CARTAS (N_BARALHOS*N_NUM_CARTAS*N_NAIPES + N_CORINGAS)	//Número total de cartas
+
 #define N_EMB 1000	//Número de trocas durante o embaralhamento
 
 typedef struct carta CARTA;
 
 CARTA *cria_baralho();	//Cria lista com as cartas e retorna ponteiro para ela
 void embaralha(CARTA *baralho);	//Embaralha as cartas
+CARTA *cria_baralho_arq(const char *nome_arquivo);	//Lê baralho de arquivo
+CARTA sacar_carta(CARTA *baralho);	//Retira carta do topo do baralho
 void swap(CARTA *a, CARTA *b);	//Troca duas cartas de posição
-//CARTA sacar_carta(*baralho);	//Retira carta do topo do baralho
+
 
 struct carta
 {
@@ -80,6 +84,90 @@ void embaralha(CARTA *baralho)
 		b = rand()%N_CARTAS;
 		swap(&(baralho[a]), &(baralho[b]));
 	}
+}
+
+CARTA *cria_baralho_arq(const char *nome_arquivo)
+{
+	//Essa função lê um baralho de um arquivo e retorna como lista.
+	//Retorna NULL caso exista problema.
+
+	FILE *fp;
+	fp = fopen(nome_arquivo, "rt");
+	if(fp==NULL)
+	{
+		fprintf(stderr, "Falha ao abrir arquivo!\n");
+		return NULL;
+	}
+
+	//Conta número de cartas que o arquivo define
+	int n_cartas = 0;
+	int c;
+	while(!feof(fp))
+	{
+		if ( (c=getc(fp)) == '\n'){n_cartas++;}
+	}
+	if(n_cartas!=N_CARTAS)
+	{
+		fprintf(stderr, "Número inválido de cartas!\n");
+		return NULL;
+	}
+	fseek(fp, 0, SEEK_SET);
+
+	//Lê as cartas
+	CARTA *baralho;
+	baralho = calloc(n_cartas, sizeof(CARTA));
+	if (baralho==NULL)
+	{
+		fprintf(stderr, "Memória cheia!\n");
+		return NULL;
+	}
+	
+	int i;
+	for (i=0; i<n_cartas && !feof(fp); i++)
+	{
+		c = getc(fp);
+		baralho[i].naipe = c;
+		c = getc(fp);
+		baralho[i].n = c;
+		c = getc(fp);
+		if(c!='\n')
+		{
+			fprintf(stderr, "Arquivo fora do formato!\n");
+			free(baralho);
+			return NULL;
+		}
+	}
+	if(feof(fp))
+	{
+		fprintf(stderr, "Arquivo fora do formato!\n");
+		free(baralho);
+		return NULL;
+	}
+	fclose(fp);
+	return baralho;
+}
+
+CARTA sacar_carta(CARTA *baralho)
+{
+	//Essa função seleciona a carta no topo do baralho, substitui-a por
+	//vazio e retorna seu valor.
+	//Retorna CARTA vazio se não houver mais cartas.
+
+	CARTA tmp;
+	CARTA vazio;	//Para saber quando a carta foi usada
+	vazio.naipe = 0;
+	vazio.n = 0;
+	int i;
+	for (i=0;i<N_CARTAS;i++)
+	{
+		tmp = baralho[i];
+		if(tmp.n!=vazio.n && tmp.naipe!=vazio.naipe)
+		{
+			baralho[i]=vazio;
+			return tmp;
+		}
+	}
+	return vazio;
 }
 
 void swap(CARTA *a, CARTA *b)
--- baralho.h
+++ baralho.h
@@ -5,6 +5,8 @@ typedef struct carta CARTA;
 
 CARTA *cria_baralho();	//Cria lista com as cartas e retorna ponteiro para ela
 void embaralha(CARTA *baralho);	//Embaralha as cartas
+CARTA *cria_baralho_arq(const char *nome_arquivo);	//Lê baralho de arquivo
+CARTA sacar_carta(CARTA *baralho);	//Retira carta do topo do baralho
 void swap(CARTA *a, CARTA *b);	//Troca duas cartas de posição
 
 struct carta
