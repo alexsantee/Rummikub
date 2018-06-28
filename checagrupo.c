018e469fc52b781689433ac1ccf378d24a228233
leo-nar <40218270+leo-nar@users.noreply.github.com>
2018-06-28 14:53:06 -0300

    correção primeira
    fiz funcionar

--- checagrupo.c
+++ checagrupo.c
@@ -1,32 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 
- typedef struct carta CARTA;
+typedef struct conjunto CONJ;
+typedef struct carta CARTA;
+struct conjunto
+{
+	CARTA *first;
+	CONJ *next;
+};
+
+
   struct carta
     {
 	char naipe;
 	char n;
 	CARTA *next;
     };
-int checagrupo(CARTA* a){//recebe um pontero para a primeira carta do grupo e retorna 0 (grupo invalido) ou 1(grupo valido)
-    int c//contador
-    if(a->n==a->next->n){
-    while(a->next,c++){
-    if(a->n==a->next->n&&c<5){
-        a=a->next;
-    }
-    else{
+int checagrupo(CONJ* d){//recebe um pontero para o grupo ORDENADO e retorna 0 (grupo invalido) ou 1(grupo valido)
+    int c=0;//contador
+	CARTA* a;
+	a=d->first;
+    if(a->n==a->next->n)
+        {
+
+        while(a->next!=NULL)
+        {
+
+            if(a->n==a->next->n&&c<4)
+            {
+                a=a->next;
+                c++;
+            }
+            else
+            {
+            return 0;
+            }
+        }
+    if(0!=c&&c!=2){
         return 0;
     }
-    }
+
 
     }
-    else{
-        while(a->next){
-        if((a->naipe==a->next->naipe)&&(a->n+1==a->next->n)){//mesmo naipe em ambos e valor do primeiro ser um antes do segundo(1  2,2 3)
-                a=a->n;
+    else{//checa sequencias
+        while(a->next!=NULL)
+            {
+        if((a->naipe==a->next->naipe)&&((a->n)+1==a->next->n))
+        {//mesmo naipe em ambos e valor do primeiro ser um antes do segundo(1  2,2 3)
+                a=a->next;
         }
-        else {
+        else
+        {
             return 0;
         }
         }
@@ -35,4 +59,3 @@ int checagrupo(CARTA* a){//recebe um pontero para a primeira carta do grupo e re
 return 1;
 
 }
-
