5bd2fb59f390f67d405edfa365812afd39c3a73d
felipearb <39956315+felipearb@users.noreply.github.com>
2018-06-28 16:02:53 -0300

    funções da mão

--- mao.c
+++ mao.c
@@ -4,35 +4,86 @@
 #include "defines.h"
 #include "baralho.h"
 
+//passar parte do add_carda_mao para a interface
+
 typedef struct mao MAO;
 
-struct mao
+struct mao//é uma listab encateada ciclica
 {
     int num_jogador;
     CARTA *first;
     MAO *next;
 };
 
-MAO inicia_mao(MAO *mao,int nj,CARTA *baralho)
+MAO *inicia_mao(int nj,CARTA *baralho)
 {
     int i;
-    for(i=0;i<nj;i++)//identificando o numero do jogador
+    MAO *mao_incial;
+    MAO *mao_atual;
+
+
+    mao_atual=calloc(1,sizeof(MAO));
+    if(mao_atual=NULL)
     {
-        mao=calloc(1,sizeof(MAO));
-        if(mao=NULL)
+        printf("Falta memoria!.");
+        exit(1);
+    }
+    mao_atual.num_jogador=0;
+
+    mao_inicial=calloc(1,sizeof(MAO));
+    if(mao_inicial=NULL)
         {
             printf("Falta memoria!.");
-            pause();
             exit(1);
         }
-        mao->num_jogador=i;
-        mao=mao.next;
+    mao_incial=mao_atual;
+
+    for(i=1;i<nj;i++)//identificando o numero do jogador
+    {add_carda_mao
+        mao_atual.next=calloc(1,sizeof(MAO));
+        if(mao_atual.next=NULL)
+        {
+            printf("Falta memoria!.");
+            exit(1);
+        }
+        *mao_atual.next.num_jogador=i;
+        mao_atual=mao_atual.next;
     }
+
+    mao_atual.next=mao_inicial;
+    mao_atual=mao_incial;
+
     for(i=0;i<nj*N_MAO_INICIAL;i++)
     {
-        mao.first=sacar_carta(baralho);
-        mao.first=mao.CARTA.next;
-        mao=mao.next;
+        mao_atual.first=sacar_carta(baralho);
+        mao_atual.first=mao_atual.first.next;
+        mao_atual=mao_atual.next;
     }
-    return
+    return mao_atual;
 }
+
+void add_carda_mao(MAO *mao,CARTA *baralho)
+{
+    CARTA carta;
+    c = mao->first;
+    while(carta.next!=NULL)
+    {
+        carta=carta.next;
+    }
+    carta.next=sacar_carta(baralho);
+    if(carta.next==NULL)
+    {
+        printf("Sem carta para comprar.\n");
+        pause();
+        clear();
+    }else
+        {
+        printf("Você comprou a carta ");
+        imprime_carta(CARTA carta);
+        putchar('\n');
+        pause();
+        clear();
+        }
+}
+
+//void remove_carta()
